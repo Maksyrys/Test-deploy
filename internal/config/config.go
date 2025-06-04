eb1001fed49f6888774803f8297696a43b49880e
FarFromMotel <maksimkapodgorniy@gmail.com>
2025-06-04 11:45:02 +0300

    confing2

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,47 +1,35 @@
 package config
 
-const (
-	dir      = "../../static"
-	host     = "db"
-	port     = "5432"
-	dbname   = "bookstore"
-	user     = "postgres"
-	password = "1111"
-	sslmode  = "disable"
-	key      = "key"
-)
+import "os"
 
 type Config struct {
 	Dir string
 	Key string
 	BD  struct {
-		Host     string
-		Port     string
-		Username string
-		Password string
-		BDName   string
-		SSLMode  string
+		Host, Port, Username, Password, BDName, SSLMode string
 	}
+}
+
+func getEnv(key, def string) string {
+	if v := os.Getenv(key); v != "" {
+		return v
+	}
+	return def
 }
 
 func NewConfig() *Config {
 	return &Config{
-		Dir: dir,
-		Key: key,
+		Dir: getEnv("DIR", "../../static"),
+		Key: getEnv("APP_KEY", "key"),
 		BD: struct {
-			Host     string
-			Port     string
-			Username string
-			Password string
-			BDName   string
-			SSLMode  string
+			Host, Port, Username, Password, BDName, SSLMode string
 		}{
-			Host:     host,
-			Port:     port,
-			Username: user,
-			Password: password,
-			BDName:   dbname,
-			SSLMode:  sslmode,
+			Host:     getEnv("BD_HOST", "db"),
+			Port:     getEnv("BD_PORT", "5432"),
+			Username: getEnv("BD_USERNAME", "postgres"),
+			Password: getEnv("BD_PASSWORD", "1111"),
+			BDName:   getEnv("BD_NAME", "bookstore"),
+			SSLMode:  getEnv("BD_SSLMODE", "disable"),
 		},
 	}
 }
