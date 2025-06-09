84587e6a16281d81069ef7778757c5d608c00b82
FarFromMotel <maksimkapodgorniy@gmail.com>
2025-06-09 22:03:05 +0300

    test4

--- internal/app/utils/utils.go
+++ internal/app/utils/utils.go
@@ -7,13 +7,13 @@ import (
 	"golang.org/x/crypto/bcrypt"
 	"html/template"
 	"net/http"
-	"path/filepath"
 )
 
 type contextKey string
 
 const UserContextKey = contextKey("currentUser")
 
+//go:embed templates/*.html
 var templatesFS embed.FS
 
 func GetCurrentUser(r *http.Request) *models.User {
@@ -39,25 +39,21 @@ func CheckPasswordHash(password, hash string) bool {
 }
 
 func Render(w http.ResponseWriter, templateFile string, data interface{}) {
-	// Извлекаем только имя файла (например "admin.html")
-	filename := filepath.Base(templateFile)
-	// Собираем путь внутри embed.FS — "templates/admin.html"
-	tplPath := filepath.Join("templates", filename)
-
-	// Парсим layout + все общие элементы + нужный шаблон
 	tmpl := template.Must(template.New("layout.html").ParseFS(templatesFS,
 		"templates/layout.html",
 		"templates/header.html",
 		"templates/modal.html",
 		"templates/footer.html",
-		tplPath,
+		templateFile,
 	))
 
 	var buf bytes.Buffer
-	if err := tmpl.ExecuteTemplate(&buf, "layout", data); err != nil {
+	err := tmpl.ExecuteTemplate(&buf, "layout", data)
+	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	buf.WriteTo(w)
+	_, _ = buf.WriteTo(w)
 }
